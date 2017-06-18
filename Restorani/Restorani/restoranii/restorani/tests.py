da0c4d7cc72c1e612c958d23998c73330d8c06b6
MirjanaLakic <mirjana.lakic9568@gmail.com>
2017-06-18 11:46:11 +0200

    uradjeni unit testovi za zaposlene

--- Restorani/Restorani/restoranii/restorani/tests.py
+++ Restorani/Restorani/restoranii/restorani/tests.py
@@ -4,6 +4,8 @@ from restorani.models import Restaurant
 from restorani.models import RestaurantManager
 from restorani.models import Employee
 from restorani.models import Waiter
+from restorani.models import Bartender
+from restorani.models import Cook
 from django.contrib.auth.models import User
 
 # Create your tests here.
@@ -15,7 +17,24 @@ class UserTestCase(TestCase):
 		user_manager = User.objects.create_user(username="mirjana@gmail.com", password='password', first_name="MANAGER")
 		manager = RestaurantManager.objects.create(name="manager", surname="lastname", email="mirjana@gmail.com",
 												   user=user_manager, restaurant=restaurant)
-
+		user_waiter = User.objects.create_user(username="waiter@gmail.com", password='password', first_name="WAITER")
+		waiter = Waiter.objects.create(name="name",
+									   surname="lastname", email="waiter@gmail.com",
+									   shoeSize=36, size="Woman S",
+									   user=user_waiter,
+									   restaurant=restaurant, firstLogin=False)
+		user_bartender = User.objects.create_user(username="bartender@gmail.com", password='password', first_name="BARTENDER")
+		bartender = Bartender.objects.create(name="name",
+									   surname="lastname", email="bartender@gmail.com",
+									   shoeSize=36, size="Woman S",
+									   user=user_bartender,
+									   restaurant=restaurant, firstLogin=False)
+		user_cook = User.objects.create_user(username="cook@gmail.com", password='password', first_name="COOK")
+		cook = Cook.objects.create(name="name", kind = "kind",
+									   surname="lastname", email="cook@gmail.com",
+									   shoeSize=36, size="Woman S",
+									   user=user_cook,
+									   restaurant=restaurant, firstLogin=False)
 	def test_user(self):
 		user = User.objects.get(username="testRestTP4@gmail.com")
 		self.assertEqual(user.first_name,"GUEST")
@@ -33,3 +52,11 @@ class UserTestCase(TestCase):
 		user_manager = User.objects.get(username="mirjana@gmail.com")
 		self.assertEqual(manager.user.first_name, "MANAGER")
 
+	def test_waiter(self):
+		self.assertIsNotNone(Employee.objects.get(email = "waiter@gmail.com"), msg=None)
+
+	def test_bartender(self):
+		self.assertIsNotNone(Employee.objects.get(email = "bartender@gmail.com"), msg=None)
+
+	def test_cook(self):
+		self.assertIsNotNone(Employee.objects.get(email="cook@gmail.com"), msg=None)
